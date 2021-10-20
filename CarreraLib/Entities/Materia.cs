0050a7e528f1c0c696a3b5870013e2d819c1e2e5
PedroT111 <pedrochincolla11@gmail.com>
2021-10-20 17:32:54 -0300

    create class materia

---
+++ CarreraLib/Entities/Materia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarreraLib.Entities
+{
+    class Materia
+    {
+        public string NombreMateria { get; set; }
+
+        public Materia()
+        {
+            NombreMateria = "";
+        }
+
+        public Materia(string nombreMateria)
+        {
+            NombreMateria = nombreMateria;
+        }
+
+        public override string ToString()
+        {
+            return NombreMateria;
+        }
+    }
+}
